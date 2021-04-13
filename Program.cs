f573b38bb1cd6110dbee6b0dd31be61f334e39cf
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-04-12 22:39:12 -0300

    Entrega atividade Divisao

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numerador, denominador, resultado;
+            bool numValido, denValido;
+        
+
+            Console.WriteLine("--Cálculo de Divisão--");
+
+            Console.Write("\nDigite o numerador....:");
+            numValido = decimal.TryParse(Console.ReadLine(), out numerador);
+
+            if(!numValido)
+            {
+                Console.WriteLine("\nDigite um numerador válido.");
+                Environment.Exit(1);
+            }
+
+            Console.Write("Digite o denominador..:");
+            denValido = decimal.TryParse(Console.ReadLine(), out denominador);
+
+            
+            if(!denValido)
+            {
+                Console.WriteLine("\nDigite um denominador válido.");
+                Environment.Exit(1);
+            }
+
+            if (denominador == 0)
+            {
+                Console.WriteLine("\nNão é possivel dividir por 0");
+            }
+            else
+            {
+                resultado = numerador / denominador;
+                Console.WriteLine($"\n{numerador} dividido por {denominador} é igual a {resultado}");
+            }
+
+        }
+    }
+}
